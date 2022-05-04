bea41e809c2da15fbd6b41f7309df2557edc501a
seyeong <seyoung7555@naver.com>
2022-05-04 15:53:09 +0900

    feat: JavaMail을 통한 메일 송신 기능 추가

--- src/main/resources/sql/user/ddl.sql
+++ src/main/resources/sql/user/ddl.sql
@@ -6,5 +6,6 @@ CREATE TABLE user (
     password varchar(10) not null,
     level tinyint not null,
     login int not null,
-    recommend int not null
+    recommend int not null,
+    email varchar(30) not null
 )
