c6a61f59f57cb167c34fe04200a3267745282bd3
seyeong <seyoung7555@naver.com>
2022-05-04 13:04:17 +0900

    feat: 스프링의 트랜잭션 추상화 기술 적용

--- src/main/java/com/toby/suntory/user/service/UserService.java
+++ src/main/java/com/toby/suntory/user/service/UserService.java
@@ -3,19 +3,26 @@ package com.toby.suntory.user.service;
 import com.toby.suntory.user.dao.UserDao;
 import com.toby.suntory.user.domain.Level;
 import com.toby.suntory.user.domain.User;
-import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
 @Component
 public class UserService {
+    @Autowired
     private UserDao userDao;
+
+    @Autowired
     private UserLevelUpgradePolicy userLevelUpgradePolicy;
+
+    @Autowired
     private DataSource dataSource;
 
     public UserService() {
@@ -33,10 +40,11 @@ public class UserService {
         this.dataSource = dataSource;
     }
 
-    public void upgradeLevels() throws SQLException {
-        TransactionSynchronizationManager.initSynchronization();
-        Connection c = DataSourceUtils.getConnection(dataSource);
-        c.setAutoCommit(false);
+    public void upgradeLevels() {
+        PlatformTransactionManager transactionManager =
+                new DataSourceTransactionManager(dataSource);
+        TransactionStatus status =
+                transactionManager.getTransaction(new DefaultTransactionDefinition());
         try {
             List<User> users = userDao.getAll();
             for (User user : users) {
@@ -44,13 +52,10 @@ public class UserService {
                     upgradeLevel(user);
                 }
             }
-        } catch (Exception e) {
-            c.rollback();
+            transactionManager.commit(status);
+        } catch (RuntimeException e) {
+            transactionManager.rollback(status);
             throw e;
-        } finally {
-            DataSourceUtils.releaseConnection(c, dataSource);
-            TransactionSynchronizationManager.unbindResource(this.dataSource);
-            TransactionSynchronizationManager.clearSynchronization();
         }
     }
 
